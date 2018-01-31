345cd45f176b30adc9da43451d9435dcd591ab69
Daniel Hristov <dhristov@live.co.uk>
2018-01-31 21:31:59 +0100

    Two more util classes for analyser status and overall frame update type.

---
+++ src/util/AnalyserStatus.java
@@ -0,0 +1,7 @@
+package util;
+
+public class AnalyserStatus {
+	public static final int COMPLETED = 1;
+	public static final int ANALYSING = 0;
+	public static final int ERROR = -1;
+}
---
+++ src/util/UpdateType.java
@@ -0,0 +1,6 @@
+package util;
+
+public class UpdateType {
+	public static final int STATISTICS = 1;
+	public static final int STATUS = 2;
+}
